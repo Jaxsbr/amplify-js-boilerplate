76f9e1292dcf25819349167a5215fea08ebd6cf9
Jaco Brink <jaco.brink@xero.com>
2023-06-06 06:07:12 +1200

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # amplify-js-boilerplate
-Boilerplate Amplify js project.
+
+After doing the Amplify tutorial a couple of times I realized I could reduce the initial creation steps by extracting a simple boilerplate project.
+
+
+## Create an **AWS IAM user** for Amplify and a local development **AWS profile**
+
+```bash
+amplify configure
+```
+
+- Login the AWS console
+- Specify region in the terminal
+- Provide an amplify IAM user. e.g. `amplify-dev`
+- Attach policies directly: `AdministratorAccess-Amplify`
+- Add an access key: `Command Line Interface (CLI)`
+- Specify the `Access key` and `Secret access key` in the terminal
+- Specify an AWS profile name. e.g. `amplify-dev`
---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const webpack = require('webpack');
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/app.js',
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/
+      }
+    ]
+  },
+  devServer: {
+    client: {
+      overlay: true
+    },
+    hot: true,
+    watchFiles: ['src/*', 'index.html'],
+    port: 7777
+  },
+  plugins: [
+    new CopyWebpackPlugin({
+      patterns: ['index.html']
+    }),
+    new webpack.HotModuleReplacementPlugin()
+  ]
+};
